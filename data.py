5f25a99531ed5816b35567e423c0b4b8137c66c9
류정관 <starlight747@naver.com>
2019-09-23 17:54:59 +0900

    Req 1-1 코드 일부 수정

--- data.py
+++ data.py
@@ -9,6 +9,7 @@ import numpy as np
 from configs import DEFINES
 
 
+FILTERS = "([~.,!?\"':;)(])"
 PAD = "<PAD>"
 STD = "<SOS>"
 END = "<END>"
@@ -20,28 +21,29 @@ END_INDEX = 2
 UNK_INDEX = 3
 
 MARKER = [PAD, STD, END, UNK]
+CHANGE_FILTER = re.compile(FILTERS)
 
 # Req 1-1-1. 데이터를 읽고 트레이닝 셋과 테스트 셋으로 분리
 def load_data():
-    Q=[]
-    A=[]
-    with open('data_in/ChatBotData.csv','r',encoding="utf-8") as f:
-        for line in f.read().splitlines():
-            Q.append(line.split(',')[0:1][0])
-            A.append(line.split(',')[1:2][0])
-    train_q,train_a,test_q,test_a=train_test_split(Q,A)
+    data_df = pd.read_csv(DEFINES.data_path, header=0)
+    question, answer = list(data_df['Q']), list(data_df['A'])
+    train_q, test_q, train_a, test_a = train_test_split(question, answer, test_size=0.33,random_state=42)
     return train_q, train_a, test_q, test_a
 
 # Req 1-1-2. 텍스트 데이터에 정규화를 사용하여 ([~.,!?\"':;)(]) 제거
 def prepro_noise_canceling(data):
-    text = re.sub('[~.,:;!\?\\\\\"\(\)\[\]\']', '', data)
+    text = re.sub(CHANGE_FILTER, '', data)
     return text
 
 # Req 1-1-3. 텍스트 데이터에 토크나이징
 def tokenizing_data(data):
-    text=prepro_noise_canceling(data)
-    text=text.split(' ')
-    return text
+    words = []
+    for sentence in data:
+        sentence = re.sub(CHANGE_FILTER, "", sentence)
+        for word in sentence.split():
+            words.append(word)
+    return [word for word in words if word]
+
 
 # Req 1-2-1. 토큰화된 트레이닝 데이터를 인코더에 활용할 수 있도록 전 처리
 def enc_processing(value, dictionary):
@@ -54,32 +56,34 @@ def enc_processing(value, dictionary):
     value = prepro_noise_canceling(value)
 
     for seq in value:
-
+        seq = re.sub(CHANGE_FILTER, "", seq)
         # 하나의 seq에 index를 저장할 배열 초기화
         seq_index =[]
 
         for word in seq.split():
             if dictionary.get(word) is not None:
+                seq_index.extend([dictionary[word]])
                 # seq_index에 dictionary 안의 인덱스를 extend 한다
                 seq_index.extend(dictionary.get(word))
             else:
+                seq_index.extend([dictionary[UNK]])
                 # dictionary에 존재 하지 않는 다면 UNK 값을 extend 한다
                 seq_index.extend(dictionary.get(UNK))
 
         # 문장 제한 길이보다 길어질 경우 뒤에 토큰을 제거
-        if len(seq_index) > DEFINES.max_sequence_length:
-            seq_index = seq_index[:DEFINES.max_sequence_length];
+        if len(sequence_index) > DEFINES.max_sequence_length:
+            seq_index = seq_index[:DEFINES.max_sequence_length]
 
         # seq의 길이를 저장
-        seq_len.append(len(seq))
+        seq_len.append(len(seq_index))
 
         # DEFINES.max_sequence_length 길이보다 작은 경우 PAD 값을 추가 (padding)
-        seq_index += (DEFINES.max_sequence_length-len(seq_index))*\[dictionary.get(PAD)]
+        seq_index += (DEFINES.max_sequence_length - len(seq_index)) * [dictionary[PAD]]
 
         # 인덱스화 되어 있는 값은 seq_input_index에 추가
         seq_input_index.append(seq_index)
 
-    return seq_input_index
+    return np.asarray(seq_input_index), seq_len
 
 # Req 1-2-2. 디코더에 필요한 데이터 전 처리
 def dec_input_processing(value, dictionary):
@@ -222,46 +226,66 @@ def load_voc():
         # 데이터를 가지고 만들어야 한다.
         # 그래서 데이터가 존재 하면 사전을 만들기 위해서
         # 데이터 파일의 존재 유무를 확인한다.
+        if (os.path.exists(DEFINES.data_path)):
+            data_df = pd.read_csv(DEFINES.data_path, encoding='utf-8')
 
-        data_df = None
-        # 판다스의 데이터 프레임을 통해
-        # 질문과 답에 대한 열을 가져 온다.
-        question, answer = None
-        data = []
-        # 질문과 답변을 extend을
-        # 통해서 구조가 없는 배열로 만든다.
-        data.extend(question)
-        data.extend(answer)
+            # 판다스의 데이터 프레임을 통해
+            # 질문과 답에 대한 열을 가져 온다.
+            question, answer = list(data_df['Q']), list(data_df['A'])
+            if DEFINES.tokenize_as_morph:  # 형태소에 따른 토크나이져 처리
+                question = prepro_like_morphlized(question)
+                answer = prepro_like_morphlized(answer)
+            data = []
+            # 질문과 답변을 extend을
+            # 통해서 구조가 없는 배열로 만든다.
+            data.extend(question)
+            data.extend(answer)
 
-        # data를 토크나이즈하여 words에 저장한다.
-        words = None
-        # 중복되는 단어(토큰)를 제거
-        words = None
+            # data를 토크나이즈하여 words에 저장한다.
+            words = data_tokenizer(data)
+            # 중복되는 단어(토큰)를 제거
+            words = list(set(words))
 
-        # 데이터 없는 내용중에 MARKER 추가
-        words[:0] = MARKER
+            # 데이터 없는 내용중에 MARKER 추가
+            words[:0] = MARKER
 
         # 사전 파일을 생성
         # DEFINES.vocabulary_path에 words안에 저장된 가 단어(토큰)들을 한줄 씩 저장
         with open(DEFINES.vocabulary_path, 'w', encoding='utf-8') as voc_file:
+            for word in words:
+                vocabulary_file.write(word + '\n')
 
     # 사전 파일에서 단어(토큰)을 가져와 voc_list에 저장
     with open(DEFINES.vocabulary_path, 'r', encoding='utf-8') as voc_file:
-
+        for line in voc_file:
+            vocabulary_list.append(line.strip())
     # make() 함수를 사용하여 dictionary 형태의 char2idx, idx2char 저장
     char2idx, idx2char = make_voc(voc_list)
 
-    return None
+    return char2idx, idx2char, len(char2idx)
 
 # Req 1-3-2. 사전 리스트를 받아 인덱스와 토큰의 dictionary를 생성
 def make_voc(voc_list):
-
-    return None
+    char2idx = {char: idx for idx, char in enumerate(vocabulary_list)}
+    idx2char = {idx: char for idx, char in enumerate(vocabulary_list)}
+    return char2idx, idx2char
 
 # Req 1-3-3. 예측용 단어 인덱스를 문장으로 변환
 def pred_next_string(value, dictionary):
+    sentence_string = []
+    is_finished = False
+    for v in value:
+        sentence_string = [dictionary[index] for index in v['indexs']]
+    answer = ""
+    for word in sentence_string:
+        if word == END:
+            is_finished = True
+            break
 
-    return None
+        if word != PAD and word != END:
+            answer += word
+            answer += " "
+    return answer, is_finished
 
 
 def main(self):
