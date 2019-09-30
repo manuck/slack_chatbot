a21bc5e04dcebd9ffd8d61329a888e2b8c631fef
류정관 <starlight747@naver.com>
2019-09-30 17:13:22 +0900

    Req. 1-5-2 함수 수정 및 주석처리

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.ERROR)
+    # tf.logging.set_verbosity(tf.logging.ERROR)
     arg_length = len(sys.argv)
 
     if (arg_length < 2):
@@ -30,40 +30,44 @@ if __name__ == '__main__':
     predic_input_enc = data.enc_processing([input], char2idx)
     # 학습 과정이 아니므로 디코딩 입력은
     # 존재하지 않는다.(구조를 맞추기 위해 넣는다.)
-    predic_output_dec = data.dec_output_processing([""], char2idx)
+    predic_input_dec = data.dec_input_processing([""], char2idx)
     # 학습 과정이 아니므로 디코딩 출력 부분도
     # 존재하지 않는다.(구조를 맞추기 위해 넣는다.)
     predic_target_dec = data.dec_target_processing([""], char2idx)
 
     # 에스티메이터 구성한다.
     classifier = tf.estimator.Estimator(
-        model_fn=ml.Model,  # 모델 등록한다.
+        model_fn=ml.model,  # 모델 등록한다.
         model_dir=DEFINES.check_point_path,  # 체크포인트 위치 등록한다.
         params={  # 모델 쪽으로 파라메터 전달한다.
             'embedding_size': DEFINES.embedding_size,
-            'model_hidden_size': DEFINES.model_hidden_size,  # 가중치 크기 설정한다.
+            'hidden_size': DEFINES.hidden_size,  # 가중치 크기 설정한다.
             'ffn_hidden_size': DEFINES.ffn_hidden_size,
             'attention_head_size': DEFINES.attention_head_size,
             'learning_rate': DEFINES.learning_rate,  # 학습율 설정한다.
             'vocabulary_length': vocabulary_length,  # 딕셔너리 크기를 설정한다.
             'embedding_size': DEFINES.embedding_size,  # 임베딩 크기를 설정한다.
             'layer_size': DEFINES.layer_size,
+            'embedding':DEFINES.embedding,
             'max_sequence_length': DEFINES.max_sequence_length,
-            'xavier_initializer': DEFINES.xavier_initializer
+            'xavier_initializer': DEFINES.xavier_initializer,
+            'multilayer': DEFINES.multilayer,
         })
 
         #TODO
 
     for i in range(DEFINES.max_sequence_length):
         if i > 0:
-            predic_output_dec, predic_output_decLength = data.dec_output_processing([answer], char2idx)
+            predic_input_dec = data.dec_input_processing([answer], char2idx)
             predic_target_dec = data.dec_target_processing([answer], char2idx)
 
-    # 예측을 하는 부분이다.
-    predictions = classifier.predict(input_fn=lambda: data.eval_input_fn(predic_input_enc, predic_input_dec, predic_target_dec, 1))
+        # 예측을 하는 부분이다.
+        predictions = classifier.predict(input_fn=lambda: data.eval_input_fn(predic_input_enc, predic_input_dec, predic_target_dec, 1))
 
-    answer, finished = data.pred_next_string(predictions, idx2char)
+        answer, finished = data.pred_next_string(predictions, idx2char)
 
+        if finished:
+            break
     # 예측한 값을 인지 할 수 있도록
     # 텍스트로 변경하는 부분이다.
     print("answer: ", answer)
